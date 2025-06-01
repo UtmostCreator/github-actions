f2c3bb3f4356176980e674931deeb910e562b662
Roman <romazahrypa@gmail.com>
2025-06-01 16:46:31 +0100

    fix: workflow test names corrected

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/test/setup.js',
-  },
+    // Add these exclude patterns:
+    exclude: [
+      '**/node_modules/**',
+      '**/tests/**',  // Exclude Playwright tests
+      '**/dist/**'    // exc to run test from production env
+    ]
+  }
 });
