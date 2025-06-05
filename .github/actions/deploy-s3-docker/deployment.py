d704e23b2835fe878711485533e6d0622d97c869
Roman <romazahrypa@gmail.com>
2025-06-05 13:46:35 +0100

    feat: custom action added to be able to use docker action

---
+++ .github/actions/deploy-s3-docker/deployment.py
@@ -0,0 +1,36 @@
+import os
+import boto3
+import mimetypes
+from botocore.config import Config
+
+
+def run():
+    # format#1 = INPUT_
+    # format#2 = YOUR_VAR_IN_UPPERCASE
+    # format#final = INPUT_YOUR_VAR_IN_UPPERCASE
+    bucket = os.environ['INPUT_BUCKET']
+    bucket_region = os.environ['INPUT_BUCKET-REGION']
+    dist_folder = os.environ['INPUT_DIST-FOLDER']
+
+    configuration = Config(region_name=bucket_region)
+
+    s3_client = boto3.client('s3', config=configuration)
+
+    for root, subdirs, files in os.walk(dist_folder):
+        for file in files:
+            # this will look for AWS_ACCESS_KEY_ID & AWS_SECRET_ACCESS_KEY (to be able to upload files)
+            s3_client.upload_file(
+                os.path.join(root, file),
+                bucket,
+                os.path.join(root, file).replace(dist_folder + '/', ''),
+                ExtraArgs={"ContentType": mimetypes.guess_type(file)[0]}
+            )
+
+    website_url = f'http://{bucket}.s3-website-{bucket_region}.amazonaws.com'
+    # The below code sets the 'website-url' output (the old ::set-output syntax isn't supported anymore - that's the only thing that changed though)
+    with open(os.environ['GITHUB_OUTPUT'], 'a') as gh_output:
+        print(f'website-url={website_url}', file=gh_output)
+
+
+if __name__ == '__main__':
+    run()
